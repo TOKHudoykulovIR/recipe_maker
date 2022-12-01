6f0457fad928a1ecb569c3d4d8eb70212d6808e8
TOKHudoykulovIR <tokhirjonbek@gmail.com>
2022-12-01 17:04:23 +0500

    4th commit

--- menu_template_sadrino/settings.py
+++ menu_template_sadrino/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5u!8yy-^^1q4uia0!x2iwj%$nvft2nw05ef-7o9pmz1%va(al#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['https://damp-taiga-59981.herokuapp.com/', '127.0.0.1']
 
